383319d52e422f19dcf90720dd559a40dd0de59e
swallace21 <shaun_wallace@brown.edu>
2022-03-13 21:11:41 -0400

    swtich urls and passages enum to ts conts to expand functionality

--- rrt/apps/study-composer/src/app/composer/passages/passages.ts
+++ rrt/apps/study-composer/src/app/composer/passages/passages.ts
@@ -1,7 +1,12 @@
-export const enum passages {
-    none,
-    eigth_grade_speed,
-    twelfth_grade_speed_comp,
-    twelfth_grade_speed_comp_trust,
-    preference
-}
+export const passages = {
+    none: '',
+    eigth_grade_speed: '',
+    twelfth_grade_speed_comp: '',
+    twelfth_grade_speed_comp_trust: '',
+    preference: ''
+} as const
+
+/* TS magic to allow flexible lookup */
+export type passages = typeof passages [
+    keyof typeof passages
+]
--- rrt/apps/study-composer/src/app/composer/urls/urls.ts
+++ rrt/apps/study-composer/src/app/composer/urls/urls.ts
@@ -3,10 +3,15 @@
 * Register the URLs for the apps that power specific studies
 *
 */
-export const enum urls {
-    instructions = '/apps/instructions',
-    preference_pairwise = '/apps/preference/pairwise',
-    preference_binary = '/apps/preference/binary',
-    reading_test_one = '/apps/reading-test-one/',
-    vrl_study_complete = '/apps/readability-testbed/vrl-study-complete',
-}
+export const urls = {
+    instructions: '/apps/instructions',
+    preference_pairwise: '/apps/preference/pairwise',
+    preference_binary: '/apps/preference/binary',
+    reading_test_one: '/apps/reading-test-one/',
+    vrl_study_complete: '/apps/readability-testbed/vrl-study-complete',
+} as const
+
+/* TS magic to allow flexible lookup */
+export type urls = typeof urls [
+    keyof typeof urls
+]
